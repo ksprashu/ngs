e8d1f50099d6ddf8e0452523d88ff7b49568ceab
Prashanth Subrahmanyam <ksprashanth82@gmail.com>
2019-01-11 19:20:37 +0530

    solving getting started challenges

--- 1-getting-started/2-timers/8-challenge2.js
+++ 1-getting-started/2-timers/8-challenge2.js
@@ -3,3 +3,13 @@
 // And stop after 5 times
 
 // After 5 times. Print "Done" and let Node exit.
+let counter = 0
+const intervalRef = setInterval(() => {
+  console.log('Hello World')
+  counter++
+
+  if (counter === 5) {
+    console.log('Done')
+    clearInterval(intervalRef)
+  }
+}, 1000)
